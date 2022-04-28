b86c02eb63dd4b78e7d767d8edd6cddc40ff3671
w6fux5 <w6fux5@gmail.com>
2022-04-28 21:26:46 +0800

    sort project basic setup

---
+++ sort/build/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const a = '1324';
+const myFunc = () => {
+  console.log('call myFunc');
+  console.log('call myFunc');
+};
+myFunc();
---
+++ sort/src/index.ts
@@ -0,0 +1,11 @@
+console.log('test');
+
+const a = '1324';
+
+const myFunc = () => {
+  console.log('call myFunc');
+  console.log('call myFunc!');
+  console.log(a);
+};
+
+myFunc();
